200ef6947d9762e3dafa5897a01222ac38c9f12b
unknown <olusanya544@gmail.com>
2025-09-10 19:55:09 +0100

    added fixes to auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,24 +1,38 @@
-import { Controller,Post,Body } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private userService: UsersService,
+  ) {}
 
-constructor(private authService:AuthService){}
+  @Post('register')
+  async register(
+    @Body() createUserDto: CreateUserDto,
+  ): Promise<{ token: string }> {
+    const token = await this.authService.register(createUserDto);
+    return { token };
+  }
 
- @Post("register")
-  async register(@Body() createUserDto:CreateUserDto):Promise<{token:string}>{
-
-  const token = await this.authService.register(createUserDto)
-    return {token}
- }
-
- @Post("login")
- async login(@Body() loginDto:LoginDto):Promise<{token:string}>{
+  @Post('login')
+  async login(@Body() loginDto: LoginDto): Promise<{ token: string }> {
     const token = await this.authService.login(loginDto);
-    return {token}
- }
-    
+    return { token };
+  }
+
+  @Get('profile')
+  @UseGuards(AuthGuard('jwt')) // Use JWT guard to protect this route
+  async profile(@Req() req: any): Promise<{ email: string }> {
+    const user = await this.userService.findUserByEmail(req.user.email);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    return { email: user.email };
+  }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 
 @Module({
@@ -22,7 +23,7 @@ import { JwtModule } from '@nestjs/jwt';
       inject:[ConfigService]
       })
   ],
-  providers: [AuthService],
+  providers: [AuthService,JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
